fca0a376bb6a5c3caac8ba8c21e5b773968189d6
Kin <kinzhangglimmer@gmail.com>
2023-10-21 13:14:20 +0200

    feat: dztimer print.

--- main_rushversion.py
+++ main_rushversion.py
@@ -15,6 +15,8 @@ np.set_printoptions(threshold=np.inf)
 from tqdm import tqdm
 import sys, os
 BASE_DIR = os.path.abspath(os.path.dirname( __file__ ))
+# pip install --index-url https://test.pypi.org/simple/ --extra-index-url https://pypi.org/simple/ dztimer
+import dztimer # only py38 can use now, working to publish on pypi
 
 # vis
 import open3d as o3d
@@ -35,9 +37,11 @@ RANGE = 40 # m, from cetner point to an square
 RESOLUTION = 1 # m, resolution default 1m
 H_RES = 0.5 # m, resolution default 1m
 
-DATA_FOLDER = f"/home/mjiaab/workspace/edo_ws/edomap_release/edomap/data/KITTI/01"
+DATA_FOLDER = f"/home/kin/code_online/edomap/data/KITTI/01/"
 MAX_RUN_FILE_NUM = -1 # -1 for all files
 
+timer = dztimer.Timing()
+timer.start("Total")
 print(f"We will process the data in folder: {bc.BOLD}{DATA_FOLDER}{bc.ENDC}")
 points_index2Remove = []
 
@@ -46,6 +50,7 @@ raw_map_path = f"{DATA_FOLDER}/raw_map.pcd"
 if not os.path.exists(raw_map_path):
     raw_map_path = f"{DATA_FOLDER}/gt_cloud.pcd"
 
+timer[0].start("Map Generation")
 Mpts = BEETree()
 Mpts.set_points_from_file(raw_map_path)
 Mpts.set_unit_params(RESOLUTION, RESOLUTION, H_RES)
@@ -56,7 +61,7 @@ Mpts.generate_map_binary_tree()
 print("Generate binary tree in Map cost: ",time.time() - t1, " s")
 Mpts.get_binary_matrix()
 Mpts.get_minz_matrix()
-
+timer[0].stop()
 print("finished M, cost: ", time.time() - t1, " s")
 
 all_pcd_files = sorted(os.listdir(f"{DATA_FOLDER}/pcd"))
@@ -65,6 +70,8 @@ for file_cnt, pcd_file in tqdm(enumerate(all_pcd_files)):
     if file_cnt>MAX_RUN_FILE_NUM and MAX_RUN_FILE_NUM!=-1:
         break
     t1 = time.time()
+    timer[5].start("One Scan Cost")
+    timer[1].start("Query BeeTree")
     Qpts = BEETree()
     Qpts.set_points_from_file(f"{DATA_FOLDER}/pcd/{pcd_file}")
     Qpts.set_unit_params(RESOLUTION, RESOLUTION, H_RES)
@@ -72,8 +79,10 @@ for file_cnt, pcd_file in tqdm(enumerate(all_pcd_files)):
 
     Qpts.matrix_order = (int)(RANGE / RESOLUTION)
     Qpts.calculate_query_matrix_start_id()
-
+    
+    t15 = time.time()
     Qpts.generate_query_binary_tree(Mpts.minz_matrix)
+    print("generate_query_binary_tree cost: ",time.time() - t15)
     Qpts.get_binary_matrix()
 
     # pre-process
@@ -83,9 +92,14 @@ for file_cnt, pcd_file in tqdm(enumerate(all_pcd_files)):
 
     binary_xor = (~Qpts.binary_matrix) & map_binary_matrix_roi
     trigger = binary_xor
+    timer[1].stop()
 
     # BGM
+    timer[2].start("BGM")
     Qpts.BlindGridMask = Qpts.generate_blind_grid_mask()
+    timer[2].stop()
+
+    timer[3].start("SRM with RV")
     # SRM
     Qpts.SightRangeMask = Qpts.generate_sight_range_mask(k, minz_matrix_roi, outlier_matrix_roi)
     trigger &= ~Qpts.SightRangeMask
@@ -94,7 +108,9 @@ for file_cnt, pcd_file in tqdm(enumerate(all_pcd_files)):
     # RV ray casting
     blocked_mask = Qpts.reverse_virtual_ray_casting(trigger, minz_matrix_roi)
     trigger &= ~blocked_mask
+    timer[3].stop()
 
+    timer[4].start("LOGIC")
     ground_index_matrix = np.log2((trigger & -trigger) >> 1).astype(int)
     ground_trigger = Mpts.calculate_ground_mask(Qpts, ground_index_matrix)
     for (i,j) in list(zip(*np.where(trigger != 0))):
@@ -108,6 +124,8 @@ for file_cnt, pcd_file in tqdm(enumerate(all_pcd_files)):
     t_tmp = time.time() - t1
     print("finished Q, cost: ", t_tmp, " s")
     t_list.append(t_tmp)
+    timer[4].stop()
+    timer[5].stop()
 
 mean_t = sum(t_list) / len(t_list)
 np_t_list = np.array(t_list)
@@ -128,5 +146,6 @@ outlier_cloud = Mpts.original_points[points_index2Retain]
 save_pcd(f"{DATA_FOLDER}/edomap_output.pcd", outlier_cloud) # static map
 
 # save_pcd(f"{DATA_FOLDER}/edomap_output_r.pcd", np.array(inlier_cloud.points))
-
+timer.stop()
+timer.print(title="BeautyMap",random_colors=True, bold=True)
 print(f"{os.path.basename( __file__ )}: All codes run successfully, Close now..")
